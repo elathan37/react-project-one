864da1da1d51e9713b855ae073c00a6311b351c2
Elgen Lathan <elgen.lathan@gmail.com>
2024-04-01 02:05:57 -0700

    fixed formatting but not done, need to resolve react-bootstrap issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Header = () => {
-    const title='Knowledge Hub';
-    console.log(title);
-  <header className="header" style={{ background: 'linear-gradient(to bottom, lightblue, pink)', padding: '1rem', height: '80px' }}>
+  const title = "Knowledge Hub";
+  console.log(title);
+  <header className="header">
     <h1 className="header-title">{title}</h1>
-    </header>
+  </header>;
 };
 
 export default Header;
